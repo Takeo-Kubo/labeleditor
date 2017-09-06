d38f54ee87f6dfbc2327076927203604ff97c49f
kubo.takeo <takeo@kik.co.jp>
2017-09-06 19:10:59 +0900

    second commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <svg version="1.1" viewBox="0 0 103.8333359 80.1666641"  width="103.3333359mm" height="79.6666641mm" style={{enableBackground: "new 0 0 103.8333359 80.1666641"}}>
+<g id="Cutline"></g>
+<g id="トンボ"></g>
+<g id="正面">
+<g id="検査項目">
+<text transform="matrix(1 0 0 1 6.25 25.6514072)" style={{fontFamily: "Oh218Pi", fontSize: "25px"}}>NEFA</text>
+<g id="商品コード">
+<text transform="matrix(1 0 0 1.28571 62.25 34.5566826)">
+  <tspan x="0" y="0" style={{fontFamily: 'Oh036Pi',fontSize: "7px"}}>276</tspan>
+  <tspan x="12.71875" y="0" style={{fontFamily: 'Oh036Pi',fontSize: "7px"}}>-</tspan>
+  <tspan x="14.793457" y="0" style={{fontFamily: 'Oh036Pi',fontSize: "7px"}}>76491</tspan>
+  </text>
+</g>
+<g id="商品名"><text transform="matrix(1 0 0 1 6.25 57.3457451)" style={{fontFamily: 'IwaGoETS-BD', fontSize: "8px"}}>遊離脂肪酸基準液</text></g>
+
+
+<rect x="0.2499962" y="0.2499911" style={{fill: "none",stroke: "#E4007F",strokeWidth: "0.5",strokeMiterlimit: "10"}} width="103.3333359" height="79.6666641"/>
+
+<g id="容量"></g>
+<g id="和光ロゴ"></g></g></g>
+</svg>
+      </div>
+    );
+  }
+}
+
+export default App;
